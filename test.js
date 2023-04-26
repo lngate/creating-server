ba851dd340c1251076efe9cc02a37c3a7c353261
Lewis Ng’ate <lewisngate@Lewiss-MacBook-Air.local>
2023-04-26 15:34:46 +0300

    trying out different server

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+let http = require("http");
+
+http.createServer(function(req, res){
+    Response.writeHead(200, {"content-type": "text/plain"});
+    Response.write("welcome to page");
+    Response.end();
+}).listen(5500);
+
+
+let http = require("http"); 
+
+let  server = http.createServer(); 
+server.listen(8000);
